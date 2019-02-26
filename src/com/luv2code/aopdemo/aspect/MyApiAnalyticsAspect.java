96a1a1eae921282f90fc0fd4d93a8479837a6048
Gret <gretccx@gmail.com>
2019-02-25 19:34:23 -0600

    Refactored to order aspects.

--- src/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -7,14 +7,11 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
+@Order(3)
+public class MyApiAnalyticsAspect {
 
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() {
-		
-		// add our custom code
-		System.out.println("\n====>>>> Executing @Before advice on addAccount()");
+	public void performApiAnalytics() {
+		System.out.println("\n====>>>> Performing API analytics");
 	}
-
 }
